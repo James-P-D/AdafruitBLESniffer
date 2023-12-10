f8d523febe237bb23388c60656a16cb002581f5e
James <jdorrington@hotmail.com>
2023-12-10 16:23:11 +0000

    Fixes for following by name, and for dumping traffic to .PCAPNG file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import time
 from SnifferAPI import Sniffer, UART
 import argparse
+import pcapng.blocks as blocks # pip install  python-pcapng
+from pcapng import FileWriter
+
 
 class bcolors:
     HEADER = "\033[95m"
@@ -17,18 +20,21 @@ class bcolors:
 mySniffer = None
 
 
-def loop(follow_name):
+def loop(follow_name, pcap_file):
     while True:
         time.sleep(0.1)
         packets = mySniffer.getPackets()
-        process_packets(packets, follow_name)
+        process_packets(packets, follow_name, pcap_file)
 
 
-def process_packets(packets, follow_name):
+def process_packets(packets, follow_name, pcap_file):
     for packet in packets:
         if packet.OK:
-            if packet.blePacket.name == follow_name:
-                print(f"{bcolors.BLUE}", end="")
+            if follow_name:
+                if packet.blePacket.name == f'"{follow_name}"':
+                    print(f"{bcolors.BLUE}", end="")
+                else:
+                    print(f"{bcolors.GREEN}", end="")
             else:
                 print(f"{bcolors.GREEN}", end="")
 
@@ -51,10 +57,33 @@ def process_packets(packets, follow_name):
             print(payload)
             index += size
 
+        if packet.OK and pcap_file:
+            shb = blocks.SectionHeader(
+                options={
+                    "shb_hardware": "artificial",
+                    "shb_os": "python",
+                    "shb_userappl": "python-pcapng",
+                }
+            )
+            idb = shb.new_member(
+                blocks.InterfaceDescription,
+                link_type=272,
+                snaplen=65535,
+                options={
+                    "if_description": "Hand-rolled",
+                    "if_os": "Python",
+                },
+            )
+
+            writer = FileWriter(pcap_file, shb)
+            spb = shb.new_member(blocks.SimplePacket)
+            spb.packet_data = b"\x10" + bytes(data)
+            writer.write_block(spb)
+
         print()
 
 
-def main(com_port, list_mode, follow_name):
+def main(com_port, list_mode, follow_name, pcap_file):
     global mySniffer
 
     mySniffer = Sniffer.Sniffer(com_port)
@@ -73,38 +102,43 @@ def main(com_port, list_mode, follow_name):
                 print(f"{addr}")
         exit()
 
-    dev = all_devices.find("" if follow_name is None else f""{follow_name}"")
+    dev = all_devices.find("" if follow_name is None else f'"{follow_name}"')
 
     if dev is not None:
+        print(f"Found BLE device named {follow_name}")
         mySniffer.follow(dev)
     else:
-        print("Could not find device")
+        print(f"Could not find BLE device named {follow_name}")
         exit()
 
     if mySniffer is not None:
-        loop(follow_name)
+        loop(follow_name, pcap_file)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser = argparse.ArgumentParser(description="Adafruit BLE Sniffer")
     parser.add_argument("-l",
                         action="store_true",
                         help="List known BLE devices")
     parser.add_argument("-c",
                         metavar="COM port",
                         type=str,
-                        nargs=1,
+                        nargs="?",
                         required="true",
                         help="COM port for Adafruit Bluefruit device")
     parser.add_argument("-n",
-                        metavar="follow name",
+                        metavar="Follow name",
                         type=str,
-                        nargs="?",
+
                         help="BLE device name to follow")
+    parser.add_argument("-p",
+                        metavar="PCAPNG output file",
+                        type=argparse.FileType("wb"),
+                        help="Path to PCAPNG file to dump packets")
     args = parser.parse_args()
 
     try:
-        main(args.c[0], args.l, args.n)
+        main(args.c, args.l, args.n, args.p)
     except KeyboardInterrupt:
         print(bcolors.RESET + "Aborting")
 
